f2a0bce6e7014315349659f34498bde5296b2c6f
atikis87 <ati.radeone@gmail.com>
2022-01-22 20:28:45 +0100

    Set vue js for the master blade

--- Sport_events_calendar/resources/js/app.js
+++ Sport_events_calendar/resources/js/app.js
@@ -1,1 +1,12 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+
+import App from './app.vue';
+
+Vue.component('app', require('./app.vue').default);
+
+const app = new Vue({
+    el: '#app',
+    components:{'app':App}
+});
---
+++ Sport_events_calendar/resources/js/app.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        <anyChild></anyChild>
+        <h1>Hello World</h1>
+    </div>
+</template>
+
+<script>
+
+import anyChild from './vue/addTableForm.vue'
+export default{
+    name: "app",
+    components: {anyChild}
+}
+</script>
